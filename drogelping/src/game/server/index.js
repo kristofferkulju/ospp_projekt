bc840fc7d5eb39864d12b31a3b202b12269c06ab
jast6407 <jakob.strandman.6407@student.uu.se>
2024-05-09 03:39:24 +0200

    Added console.log comments for each socket.on (clarifying what does what)

--- drogelping/src/game/server/index.js
+++ drogelping/src/game/server/index.js
@@ -20,43 +20,37 @@ const io = new Server(server, {
 
 io.on("connection", (socket) => {
 
-    console.log(`Game server User connected: ${socket.id}`);
+    console.log(`[CONNECTION_ESTABLISHED](${socket.id})`); // SocketChat, SocketGame
 
-    socket.on("join_room", (data) => {
+    socket.on("join_room", (data) => { // SocketChat
         socket.join(data);
-        console.log(`User with id: ${socket.id} Joined room: ${data}`);
+        console.log(`[JOIN_ROOM](${socket.id}): (${data})`);
     });
 
-    socket.on("send_message", (data) => {
-        socket.to(data.room).emit("receive_message", data);
-        console.log(`This is the message: ${data.message} With room: ${data.room}`);
+    socket.on("send_message", (data) => { // SocketChat (messages), SocketGame (goals)
+        socket.to(`${data.room}`).emit("receive_message", data);
+        console.log(`[SEND_MESSAGE](${socket.id}): ${data.author}(${data.room}) - "${data.message}"`);
     });
 
-    socket.on("update_position", (data) => {
-        console.log("Updated position");
+    socket.on("update_position", (data) => { // SocketGame (paddle position)
         socket.join("123");
         socket.to("123").emit("update_position", data);
-
+        console.log(`[UPDATE_POSITION]: *${data}*`);
     });
 
     socket.on("sync_ball", (data) => {
-        console.log("Synced ball");
         socket.to("123").emit("sync_ball", data);
+        console.log(`[SYNC_BALL]`);
     });
 
     socket.on("sync_paddle", (data) => {
-        console.log("Synced paddle");
         socket.to("123").emit("sync_paddle", data);
+        console.log(`[SYNC_PADDLE]: *${data}*`);
     });
 
     socket.on("update_score", (data) => {
-        console.log("Goal gg wp");
         socket.to("123").emit("sync_score", data);
-    });
-
-    socket.on("GOAL", (data) => {
-        socket.to(data.room).emit("receive_message", data);
-        console.log(`This is the message: ${data.message} With room: ${data.room}`);
+        console.log(`[UPDATE_SCORE]: *${data}*`);
     });
 });
 
